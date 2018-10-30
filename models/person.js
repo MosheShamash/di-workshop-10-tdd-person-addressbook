c99157caf9d59fdafceac2c2fdc3d2e172c69130
Moshe Shamash <mshamash@fb.com>
2018-10-30 11:40:41 +0000

    finished first part

--- models/person.js
+++ models/person.js
@@ -1,3 +1,18 @@
+class Person {
+    constructor(firstName, surname, dob) {
+        this.firstName = firstName[0].toUpperCase() + firstName.slice([1])
+        this.surname = surname[0].toUpperCase() + surname.slice([1])
+        this.dob = dob
+        this.emails = []
+    }
 
+    fullName() {
+        return this.firstName + " " + this.surname
+    }
 
-class Person
+    addEmail(email) {
+        this.emails.push(email)
+    }
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,8 +1,32 @@
 const chai = require('chai')
 const expect = chai.expect
 
+const Person = require('../models/person.js')
+
 describe('Person', ()=> {
     it('Should inititalize properly', ()=> {
-        var person = new Person('Jonathan', 'Rafaeli')
+        var person = new Person('Jonathan', 'Rafaeli', '1 Jan 1990')
+        expect(person.firstName).to.equal('Jonathan')
+        expect(person.surname).to.equal('Rafaeli')
+        expect(person.dob).to.equal('1 Jan 1990')
+        expect(person.emails).to.deep.equal([])
+    })
+
+    it('Should capitaliaze first name and surname properly', ()=> {
+        var person = new Person('jonathan', 'rafaeli', '1 Jan 1990')
+        expect(person.firstName).to.equal('Jonathan')
+        expect(person.surname).to.equal('Rafaeli')
+    })
+
+    it('Should return a full name', ()=> {
+        var person = new Person('jonathan', 'rafaeli', '1 Jan 1990')
+        expect(person.fullName()).to.equal('Jonathan Rafaeli')
+    })
+
+    it('Should accept new emails and add them to the array', ()=> {
+        var person = new Person('jonathan', 'rafaeli', '1 Jan 1990')
+        person.addEmail('joe@gmail.com')
+        person.addEmail('mo@gmail.com')
+        expect(person.emails).to.deep.equal(['joe@gmail.com', 'mo@gmail.com'])
     })
 })
